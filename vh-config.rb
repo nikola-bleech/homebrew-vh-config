fd5d21b035226ab05721179092cb337025c71c57
nikola-bleech <49150551+nikola-bleech@users.noreply.github.com>
2019-06-25 11:59:17 +0200

    Update SHA

--- vh-config.rb
+++ vh-config.rb
@@ -2,7 +2,7 @@ class VhConfig < Formula
   desc "Config generator for Apache Virtual Host"
   homepage ""
   url "https://github.com/nikola-bleech/vh-config/archive/v0.1.4.tar.gz"
-  sha256 "7c933282b479dd80efa54f19cb527ec3c3a49dcc5ef5c8dff362385521a5546a"
+  sha256 "4b86fdd0ae2076f4d064165b0e7a12be332cc73da074a2563241c2e98409d20e"
   version "0.1.4"
   # depends_on "cmake" => :build
 

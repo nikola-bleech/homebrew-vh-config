1bc16bb06f9b2e638523f8b4a2c8323fd3b52126
nikola-bleech <49150551+nikola-bleech@users.noreply.github.com>
2019-06-25 11:53:08 +0200

    Update version number.

--- vh-config.rb
+++ vh-config.rb
@@ -1,12 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class VhConfig < Formula
   desc "Config generator for Apache Virtual Host"
   homepage ""
-  url "https://github.com/nikola-bleech/vh-config/archive/v0.0.3.tar.gz"
+  url "https://github.com/nikola-bleech/vh-config/archive/v0.1.4.tar.gz"
   sha256 "7c933282b479dd80efa54f19cb527ec3c3a49dcc5ef5c8dff362385521a5546a"
-  version "0.0.3"
+  version "0.1.4"
   # depends_on "cmake" => :build
 
   def install

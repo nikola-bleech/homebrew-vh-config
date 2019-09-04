d9ae689e2237c5c002c80a55cefc6bf4f2f577b3
Nikola Jovanovic <nikola@bleech.de>
2019-09-04 11:55:03 +0200

    Update to v0.3.0

--- vh-config.rb
+++ vh-config.rb
@@ -1,9 +1,9 @@
 class VhConfig < Formula
   desc "Config generator for Apache Virtual Host"
   homepage ""
-  url "https://github.com/nikola-bleech/vh-config/archive/v0.2.1.tar.gz"
-  sha256 "b9e55f21e51e52b310816f9d2f8630fbdb0a98d3aed196d02e37724280342c61"
-  version "0.2.1"
+  url "https://github.com/nikola-bleech/vh-config/archive/v0.3.0.tar.gz"
+  sha256 "98769a8b80cacfa01000d6b9664716f6a9ec26f328fc67b323624032d78785dd"
+  version "0.3.0"
   # depends_on "cmake" => :build
 
   def install
